542eb1d62c8a961c11ac2c920de5c731316ba381
Nagarjun <nagarjun.ms@hashedin.com>
2017-01-17 11:26:02 +0530

    Revert "Revert Update "README.md""

--- test_simple.py
+++ test_simple.py
@@ -1,17 +1,3 @@
-def abc(request):
-  print "New Line"
-  print "New Line"
-  print "New Line"
-  print "New Line"
-  print "New Line"
-  print "New Line"
-  print "New Line"
-  
-def newmethod(request):
-  x= 125
-  x= 125
-  x= 125;
-  x= 125;
-  x= 125;
-  x= 125;
-  
+#aabab
+nlkdsna;las
+asnkdnsaklsa
