4c14f881def024e109b08f636f691d01515fab57
Nagarjun <nagarjun.ms@hashedin.com>
2017-01-30 19:16:39 +0530

    Revert "Update test_api.py"

--- test_api.py
+++ test_api.py
@@ -4,10 +4,3 @@ adjksandasl
 
 def new_method():
   print "Lets test the lines of code"
-
-def new_method_A():
-  print "Lets test the lines of code"
-
-def new_method_b():
-  print "Lets test the lines of code"
-  
